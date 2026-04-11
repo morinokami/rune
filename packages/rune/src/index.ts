b61549676d2cf71a705da54eecbc97630e7126c9
Shinya Fujino <shf0811@gmail.com>
2026-04-12 00:26:38 +0900

    refactor(rune): minimize public API surface

--- packages/rune/src/index.ts
+++ packages/rune/src/index.ts
@@ -1,31 +1,11 @@
 export { CommandError, defineCommand, defineGroup } from "@rune-cli/core";
 export type {
-  ArgumentHelpEntry,
   CommandArgField,
   CommandContext,
   CommandHelpData,
   CommandOptionField,
-  CommandOutput,
-  DefineGroupInput,
-  DefinedCommand,
-  DefinedGroup,
-  FrameworkOptionHelpEntry,
-  OptionHelpEntry,
-  PrimitiveArgumentHelpEntry,
-  PrimitiveFieldType,
-  PrimitiveArgField,
-  PrimitiveOptionField,
-  PrimitiveOptionHelpEntry,
-  SchemaArgField,
-  SchemaArgumentHelpEntry,
-  SchemaOptionField,
-  SchemaOptionHelpEntry,
-  SubcommandHelpEntry,
-  UserOptionHelpEntry,
 } from "@rune-cli/core";
 
 export { defineConfig } from "./define-config";
-export type { RuneConfig, RuneConfigInput } from "./define-config";
-
 export { renderDefaultHelp } from "./manifest/runtime/render-help";
 export type { GroupHelpData, HelpData, UnknownCommandHelpData } from "./manifest/runtime/help-data";
